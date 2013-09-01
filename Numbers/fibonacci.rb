0a7a9d2d7c4895cb4c7475a84e1a5555bf2169f2
Philipp Hansch <dev@phansch.net>
2013-09-01 17:40:54 +0200

    Added fibonacci project

---
+++ Numbers/fibonacci.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# limit - the amount of fibonacci numbers to be returned
+def fibonacci(limit)
+    a = [0, 1]
+    until a.length == limit
+	    n = a[a.length-1] + a[a.length-2]
+	    a.insert(a.length, n)
+	end
+	return a.to_s
+end
+
+puts fibonacci(10)
